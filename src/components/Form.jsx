add75d008f9e016ebc30219ed8f9b7538bc562de
sharmin-khan <sharminkhancse@gmail.com>
2025-11-18 03:12:35 +0600

    chore: implement addRow function for dynamic form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import FormRow from "./FormRow";
 
-const form = () => {
+const Form = () => {
+
+    const [rows, setRows]=useState([{}]);// initially empty row
+
+    const addRow=()=>{
+        setRows([...rows,{}]);//add new row
+    }
+
   return (
     <div className="max-w-md mx-auto mt-10 p-4 border rounded bg-white">
       <h1 className="text-xl font-bold mb-4">Simple Form</h1>
      
       <form>
-          <FormRow />
+        {rows.map((row,index)=>(
+          <FormRow key={index} />
+        ))}
           <button
-          type="button"className="bg-green-500 text-white px-3 py-1 rounded mt-2">
+          type="button"
+          onClick={addRow}
+          className="bg-green-500 text-white px-3 py-1 rounded mt-2">
             +
           </button>
            <button
@@ -23,5 +34,4 @@ const form = () => {
     </div>
   );
 };
-
-export default form;
+export default Form;
